89dbd995c033c1cc4504cb953cd61001088979e2
“Sahitya <sahityakarn@email.com>
2024-06-07 08:56:23 +0530

    2nd programme

---
+++ 2-variable.py
@@ -0,0 +1,14 @@
+name="neha"
+course="python"
+roll=101
+fee=850.3234566
+
+
+print("Your name is \t:\t",name)
+print("Your course is \t:\t",course)
+print("Your roll is \t:\t",roll)
+print("Your fee is \t:\t",fee)
+
+#print("hello ",name)
+#string "sahitya" interger 101 float 112.45 decimal 222.45677
+print(type(fee))
