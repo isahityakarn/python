a333057a82b9cfdef19fe9b590cdd3d90a90c174
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-08 11:51:07 +0530

    asas

---
+++ second.py
@@ -0,0 +1,13 @@
+num=54
+num1=27
+
+total=num+num1
+diff=num-num1
+product=num*num1
+division=num/num1
+
+
+print("the total is \t  : ",total)
+print("the difference is : ",diff)
+print("the product is \t  : " ,product)
+print("the quotient is   : ",division) 
---
+++ third.py
@@ -0,0 +1,8 @@
+num1=15
+num2=2
+num3=34
+total=num1+num2+num3
+
+print("the total is   : ",total)
+
+
