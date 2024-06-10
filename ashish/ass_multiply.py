cd9ab293a26558db5852837138f5dd570cb4487b
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-10 12:09:34 +0530

    assignment

---
+++ ashish/ass_multiply.py
@@ -0,0 +1,5 @@
+num1=5
+num2=3
+
+num1*=num2
+print("the assignment value is :",num1)
---
+++ ashish/ifcondition.py
@@ -0,0 +1,10 @@
+age=67
+
+if age>=60:
+    print("old")
+elif age>=20:
+    print("adult")
+elif age>=13:
+    print("teen")
+else :
+    print("child")  
