b0e7b2d0179def1a60b70ca4857a721f5b094e7d
“Sahitya <sahityakarn@email.com>
2024-06-10 11:27:31 +0530

    if ocnditon

---
+++ ifcondtin.py
@@ -0,0 +1,18 @@
+#if age is >17 adulit else child
+#if age is >50 old if age >35 adult if age>16 teen adulit else child
+age=47.65
+# if age>=18:
+#     print("adult")
+# else:
+#     print("child")
+
+
+
+if age>=50:
+    print("old")
+elif age>=35:
+    print("audit")
+elif age>16:
+    print("teen")
+else:
+    print("child")
