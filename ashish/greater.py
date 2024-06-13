60ba829b2fb74142299b3f1e9f426dd4901f39d7
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-13 12:02:44 +0530

    iuewhiu

--- ashish/greater.py
+++ ashish/greater.py
@@ -1,8 +1,12 @@
-num1=12
-num2=10
-num3=11
+num1=127
+num2=199
+num3=99
 
-if num1>num2:
-    print("big value is : ")
-else:
-    print("num1 < num2")
+if num1>num2>num3:
+    print("big value is :",num1)
+elif num2>num1>num3:
+    print("big value is :",num2)
+elif num3>num2>num1:
+    print("big value is :",num3)
+elif num2>num3>num1:
+    print("big value is :",num2)
