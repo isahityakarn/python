bc76c2517334fcbf5a1ee8fed38aa958dc6598f8
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-13 12:10:34 +0530

    shu

--- ashish/greater.py
+++ ashish/greater.py
@@ -1,6 +1,6 @@
-num1=127
-num2=199
-num3=99
+num1=999
+num2=1030
+num3=980
 
 if num1>num2>num3:
     print("big value is :",num1)
@@ -10,3 +10,7 @@ elif num3>num2>num1:
     print("big value is :",num3)
 elif num2>num3>num1:
     print("big value is :",num2)
+elif num1>num3>num2:
+    print("big value is :",num1)
+elif num3>num1>num2:
+    print("big value is :",num3)
