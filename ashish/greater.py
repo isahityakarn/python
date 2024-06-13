0e3ec4d9a847895f8811882b7fa7f6c6d6880068
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-13 12:30:06 +0530

    aiuhi

--- ashish/greater.py
+++ ashish/greater.py
@@ -1,6 +1,6 @@
-num1=999
-num2=1030
-num3=980
+num1=5
+num2=1
+num3=5
 
 if num1>num2>num3:
     print("big value is :",num1)
@@ -14,3 +14,17 @@ elif num1>num3>num2:
     print("big value is :",num1)
 elif num3>num1>num2:
     print("big value is :",num3)
+elif num1>num2==num3:
+    print("big value is :",num1)
+elif num2>num1==num3:
+    print("big value is :",num2)
+elif num3>num1==num2:
+    print("big value is :",num3)
+elif num1==num2>num3:
+    print("big value is :",num1)
+elif num2==num3>num1:
+    print("big value is :",num2)
+elif num3==num1>num2:
+    print("big value is :",num3)
+else :
+    print("all values are same")
