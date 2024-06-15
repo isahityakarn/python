8fc37ed17dd8f562a36cdb04d96a5735c3c6e216
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-15 12:09:02 +0530

    iurhm

--- ashish/greater.py
+++ ashish/greater.py
@@ -1,10 +1,13 @@
-num1=1
-num2=6
-num3=9
+num1=1000
+num2=698
+num3=987
+num4=1254
 
-if num1>=num2 and num1>=num3:
+if num1>=num2 and num1>=num3 and num1>=num4:    
     print("the biggest no. is :",num1)
-elif num2>=num3 and num2>=num1:
+elif num2>=num3 and num2>=num4:
     print("the biggest no. is :",num2)
-else :
+elif num3>=num4:
     print("the biggest no. is :",num3)
+else :
+    print("the biggest no. is :",num4)
