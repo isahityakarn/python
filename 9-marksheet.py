53476175b169a13bc064d79b3f69937b10f9aeb3
“Sahitya <sahityakarn@email.com>
2024-06-17 11:31:53 +0530

    mytest

---
+++ 9-marksheet.py
@@ -0,0 +1,3 @@
+#input: name,hindi,eng,math
+#output : your name is : ,your hindi mark is : , your Eng mark is: ,your math marks is : , your total is : , your average is : ,
+#avg>60 result: 1st, avg>45 result:2nd, avg<=45 result 3rd,
