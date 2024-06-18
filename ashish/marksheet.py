3aca3340900e6ee01deddbe05db0bd873a1f95dd
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-18 11:28:44 +0530

    yucv

--- ashish/marksheet.py
+++ ashish/marksheet.py
@@ -2,7 +2,6 @@ name=input("name \t    : ")
 mark1=int(input("hindi marks : "))
 mark2=int(input("eng marks   : "))
 mark3=int(input("math marks  : "))
-print("your name is       :",name)
 print("your hindi mark is :",mark1)
 print("your eng mark is   :",mark2)
 print("your math mark is  :",mark3)
@@ -12,7 +11,11 @@ avg=total/3
 print("your average is\t   :",avg)
 if avg>60 :
     print("1st")
-elif avg<60 and avg>45:
+elif avg>45:
     print("2nd")
 else :
     print("3rd")
+if mark1>=33 and mark2>=33 and mark3>=33:
+    print("pass")
+else :
+    print("fail")
