8f34342614d4825f97237b27bf6c22bfc0e056c8
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-19 11:21:49 +0530

    udi

--- ashish/salaryslip.py
+++ ashish/salaryslip.py
@@ -15,8 +15,7 @@ print("Your gross salary is :",Gross_salary)
 Net_salary=Gross_salary-PF-ESI
 print("Your net salary is   :",Net_salary)
 Annual_salary=Net_salary*12
-IT=(Annual_salary-300000)*10/100
 if Annual_salary>300000:
-    print("IT : ",IT)
+    print("IT  :",(Annual_salary-300000)*10/100)
 else :
     print("NO IT")
