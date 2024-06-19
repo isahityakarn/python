6394d2a9a04eba70e6acfebaa94ebc9224d12c6e
“Sahitya <sahityakarn@email.com>
2024-06-19 11:25:29 +0530

    while loop

---
+++ 10-whileloop.py
@@ -0,0 +1,21 @@
+i = int(input("Enter values to start loop"))
+e = int(input("Enter values to close loop"))
+while i <= e:
+  print(i)
+  i += 1
+
+#break  
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+#contitune
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
