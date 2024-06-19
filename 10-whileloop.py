6f235cffa8a56b4d080d835418741e5776b2222a
“Sahitya <sahityakarn@email.com>
2024-06-19 11:29:09 +0530

    while work

--- 10-whileloop.py
+++ 10-whileloop.py
@@ -1,3 +1,9 @@
+#loop while loop
+#syntex :
+# while condition:
+#     statement
+
+
 i = int(input("Enter values to start loop"))
 e = int(input("Enter values to close loop"))
 while i <= e:
@@ -5,17 +11,17 @@ while i <= e:
   i += 1
 
 #break  
-i = 1
-while i < 6:
-  print(i)
-  if i == 3:
-    break
-  i += 1
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 3:
+#     break
+#   i += 1
 
 #contitune
-i = 0
-while i < 6:
-  i += 1
-  if i == 3:
-    continue
-  print(i)
+# i = 0
+# while i < 6:
+#   i += 1
+#   if i == 3:
+#     continue
+#   print(i)
