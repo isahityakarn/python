6b818005d612e116573ab3cf93c8daa4387de65a
rajkumar8920 <rajkumar9899608@gmail.com>
2024-06-19 11:51:20 +0530

    jgeb

---
+++ ashish/even _odd_loop.py
@@ -0,0 +1,5 @@
+i=int(input("Enter value to start loop :"))
+e=int(input("Enter value to end loop   :"))
+while i<=e:
+    print(i)
+    i+=2
---
+++ ashish/whileloop.py
@@ -0,0 +1,6 @@
+x=int(input("Enter value to start loop :"))
+y=int(input("Enter value to end loop   :"))
+while x<=y:
+    print(x)
+    x+=1
+    
