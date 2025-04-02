812cb59f2c6e66c8f150b0341a2cd2507163158d
sahitya karn <sahityakarn@gmail.com>
2025-04-02 18:57:21 +0530

    all list work

--- sahityalist.py
+++ sahityalist.py
@@ -2,3 +2,36 @@ list=['apple','mango','banana','graps','ram']
 # print (list[1])
 for i in list:
     print(i)
+
+
+    thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3)
+
